ab44334a81eed8675f6d05ca9b9fd83c831315e9
Sledge777 <bogdan.kudryashov.06@mail.ru>
2023-12-03 13:10:18 +0300

    'add_some_refs_and_curs_selector'

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <template>
   <the-header/>
-  <nav>
-        
-  </nav>
+  <p>направления</p>
+  <section class="chose">
+    <div class="chose__wrapper">
+      <img class="chose__wrapper__img" src="@/assets/webdev.jpg" alt="">
+      <div class="btn"></div>
+      <div class="btn2"></div>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -24,5 +29,39 @@ export default {
 }
 body {
   background-color: black;
+}
+.chose {
+  width: 50%;
+  margin: auto;
+}
+.chose__wrapper {
+  position: relative;
+}
+.chose__wrapper__img {
+  width: 100%;
+  border-radius: 50px;
+}
+.btn {
+  height: 30px;
+  width: 30px;
+  background-color: black;
+  position:absolute;
+  border-radius: 100%;
+  bottom:10px;
+  right: 20px;
+}
+.btn2 {
+  height: 30px;
+  width: 30px;
+  background-color: black;
+  position:absolute;
+  border-radius: 100%;
+  bottom:10px;
+  right: 60px;
+}
+.btn2:hover, .btn:hover {
+  background-color: grey;
+  transition: all 0.3s;
+  cursor: pointer;
 }
 </style>
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,83 +1,26 @@
 <template>
   <header>
-    <div class="logo">
-      <img src="@/assets/RGSU_logo.png" alt="Logo RGSU" class="logo__img" />
-      <div class="logo__line"></div>
-      <h1 class="logo__text">
-        ФИЛИАЛ РГСУ В Г. ПЯТИГОРСКЕ СТАВРОПОЛЬСКОГО КРАЯ
-      </h1>
-      <section>
-        
-        <ul class="refs">
-          <li>
-            <a class="refs__adv" href="mailto:pjatigorsk@rgsu.net"
-              ><strong>pjatigorsk@rgsu.net</strong></a
-            >
-          </li>
-          <li>
-            <a class="refs__adv" href="tel:+7 (8793) 33-60-97"
-              >+7 (8793) 33-60-97</a
-            >
-          </li>
-          <li>
-            <a class="refs__adv" href="tel:+7 (928) 348-84-84"
-              >+7 (928) 348-84-84</a
-            >
-          </li>
-          <li>
-            <a class="refs__adv" href="tel:+7 (495) 255-67-67"
-              >+7 (495) 255-67-67, доб. 7115</a
-            >
-          </li>
-        </ul>
-      </section>
-    </div>
+    <base-logo>
+        <header-refs/>
+    </base-logo>
   </header>
 </template>
 
-<script></script>
+<script>
+import HeaderRefs from '@/components/HeaderRefs.vue'
+import BaseLogo from '@/components/BaseLogo.vue'
+export default {
+  components: {
+    HeaderRefs,BaseLogo
+  }
+}
+</script>
 
 <style scoped>
 header {
   width: 80%;
   margin: auto;
   margin-top: 1%;
-}
-
-.logo {
-  display: flex;
-  justify-content: center;
-}
-
-.logo__img {
-  width: 40%;
-  align-self: center;
-}
-
-.logo__line {
-  width: 0.3%;
-  background-color: #024a7a;
-}
-
-.logo__text {
-  font-size: 1.5em;
-  color: #024a7a;
-  width: 30%;
-  text-align: center;
-  align-self: center;
-}
-
-.refs {
-  color: #024a7a;
-  list-style: none;
-  font-size: 1.5em;
-  align-self: center;
-  margin-left: 2%;
-  line-height: 1.2;
-}
-
-.refs__adv {
-  text-decoration: none;
-  color: #024a7a;
+  margin-bottom: 50px;
 }
 </style>
